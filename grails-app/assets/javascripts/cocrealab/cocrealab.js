7fbc145e25bb38024d678652280c90f3a13c4933
juancafro <juancsj@hotmail.com>
2016-11-09 16:32:53 -0500

    modificacion de la app creada con perfil angular 2, y con driver para MySQL no funciona pluggin de scaffolding por causa desconocida(no resuelve dependencia  del build.gradle) y la conexion a la base de datos externa no se puede realizar a pesar de haber instalado los drivers y todas las dependencias(UnknowHostException)

--- grails-app/assets/javascripts/cocrealab/cocrealab.js
+++
@@ -1,9 +0,0 @@
-//= wrapped
-//= require /angular/angular
-//= require /cocrealab/core/cocrealab.core
-//= require /cocrealab/index/cocrealab.index
-
-angular.module("cocrealab", [
-        "cocrealab.core",
-        "cocrealab.index"
-    ]);
--- grails-app/domain/org/cocrealab/Campus.groovy
+++
@@ -1,7 +0,0 @@
-package org.cocrealab
-
-class Campus {
-    Integer id
-    static belongsTo=[ies: IES, municipio:Municipio]
-    static hasMany = [programas:Programa_Campus]
-}
